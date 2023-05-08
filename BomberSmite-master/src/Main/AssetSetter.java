20d1cfb7246f2860e01260405ff67eb7f79f8b56
Doncan00 <90412543+Doncan00@users.noreply.github.com>
2023-05-07 17:32:36 -0700

    Add files via upload

--- BomberSmite-master/src/Main/AssetSetter.java
+++ BomberSmite-master/src/Main/AssetSetter.java
@@ -10,22 +10,24 @@ public class AssetSetter {
 	}
 	
 	public int colocarObjeto() {
+		int jugadorX = admJuego.jugador.x +4;
+		int jugadorY = admJuego.jugador.y +8;
 		int cont = 0;
 		for (int i=1;i<admJuego.obj.length;i++) {
 			for (int j=1;j<admJuego.obj.length;j++) {
 				if (admJuego.obj[j] != null) {
-					if ((admJuego.jugador.x / admJuego.tamPantalla) == (admJuego.obj[j].MundoX / admJuego.tamPantalla) ||
-							(admJuego.jugador.x / admJuego.tamPantalla) == (admJuego.obj[j].MundoY / admJuego.tamPantalla)) {
+					if ((jugadorX / admJuego.tamPantalla) == (admJuego.obj[j].MundoX / admJuego.tamPantalla) &&
+							(jugadorY / admJuego.tamPantalla) == (admJuego.obj[j].MundoY / admJuego.tamPantalla)) {
 						cont++;
 					}
 				}
 			}
 			if (admJuego.obj[i] == null && cont == 0) {
-				int x = admJuego.jugador.x / admJuego.tamPantalla;
-				int y = admJuego.jugador.y / admJuego.tamPantalla;
+				int x = (jugadorX)/ admJuego.tamPantalla +1;
+				int y = (jugadorY)/ admJuego.tamPantalla +1;
 				admJuego.obj[i] = new OBJ_Bomba();
-				admJuego.obj[i].MundoX = x * admJuego.tamPantalla;
-				admJuego.obj[i].MundoY = y * admJuego.tamPantalla;
+				admJuego.obj[i].MundoX = (x * admJuego.tamPantalla);
+				admJuego.obj[i].MundoY = (y * admJuego.tamPantalla);
 				cont=0;
 				return i;
 			}

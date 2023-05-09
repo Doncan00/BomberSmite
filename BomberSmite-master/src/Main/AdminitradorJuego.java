ce588cae96bbeb8ada364bac0e6bd43fea62504c
Francisco Alcantar <francisco_18_16@outlook.com>
2023-05-08 18:52:26 -0700

    Sonido fondo y muerte.

--- BomberSmite-master/src/Main/AdminitradorJuego.java
+++ BomberSmite-master/src/Main/AdminitradorJuego.java
@@ -1,11 +1,15 @@
 package Main;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.*;
 import javax.swing.*;
 
 import Objetos.SuperObjeto;
@@ -15,6 +19,10 @@ public class AdminitradorJuego extends JPanel implements Runnable {
 	//CONFIGURACIONES DE PANTALLA
 	final int escalaOriginal = 5;
 	final int escala = 5;
+
+	public int segundos=0;
+
+	public int ms=0,ms2=0;
 
 	public final int tamPantalla = escalaOriginal * escala; //25x25 CUADROS
 	public final int maxColPantalla = 40;
@@ -37,6 +45,11 @@ public class AdminitradorJuego extends JPanel implements Runnable {
 
 	public Enemigo enemigo2 = new Enemigo(this);
 
+	public Clip clip,clipm;
+	public AudioInputStream audioInputStream,maudioInputStream;
+
+	public int muerto=0;
+
 	public Enemigo enemigo3 = new Enemigo(this);
 
 	public Vidas vidascantidad = new Vidas(3);
@@ -48,7 +61,7 @@ public class AdminitradorJuego extends JPanel implements Runnable {
 		for (int i=0; i<expl.length; i++) {
 			expl[i] = false;
 		}
-
+		ReproductorDeSonido();
 		enemigo1.setEy(425);
 		enemigo2.setEy(425);
 		enemigo2.setEx(925);
@@ -92,12 +105,62 @@ public class AdminitradorJuego extends JPanel implements Runnable {
 					//1 ACTUALIZA: ACTUALIZA LA INFORMACION COMO LA POSICION DEL PERSONAJE
 					actualizar();
 
+					ms++;
+					if(ms==60){
+						segundos++;
+						ms=0;
+					}
+					if(segundos==175){
+
+						try {
+							clip.stop();
+							clip.close();
+							audioInputStream.close();
+							ReproductorDeSonido();
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
+
+
+						segundos=0;
+					}
+					if(muerto==1){
+						SonidoMuerte();
+						try {
+
+							Thread.sleep(3000);
+							if (vidascantidad.vidasnum==0){
+								vidascantidad.vidasnum=3;
+								System.exit(0);
+							}else {
+								ReproductorDeSonido();
+								segundos = 0;
+								ms = 0;
+								clipm.stop();
+								clipm.close();
+								maudioInputStream.close();
+							}
+
+							enemigo1.evelocidad=1;
+							enemigo3.evelocidad=1;
+							enemigo2.evelocidad=1;
+							jugador.velocidad=2;
+							muerto=0;
+						} catch (Exception e) {
+							throw new RuntimeException(e);
+						}
+
+
+					}
+
+
 					
 					//2 DRAW: DIBUJA EN PANTALLA LA ACTUALIZACION DE INFORMACION
 					repaint();
 					
 				//CONTROL DE FPSSSSSSSSSS 2
 				try {
+
 					double tiempoRestante = tiempoDibujado - System.nanoTime();
 					tiempoRestante = tiempoRestante/1000000;
 					
@@ -127,19 +190,9 @@ public class AdminitradorJuego extends JPanel implements Runnable {
 		Rect enem2 = new Rect(enemigo2.ex, enemigo2.ey, 15, 15);
 		Rect enem3 = new Rect(enemigo3.ex, enemigo3.ey, 15, 15);
 		if(jug.colision(enem1) || jug.colision(enem2) || jug.colision(enem3)){
-			jugador.x=25;
-			jugador.y=25;
-			vidascantidad.vidasnum--;
-			enemigo1.setEy(425);
-			enemigo2.setEy(425);
-			enemigo2.setEx(925);
-			enemigo3.setEx(925);
+			muerto=1;
 		}
-		if (vidascantidad.vidasnum==0){
-			JOptionPane.showMessageDialog(null,"PERDISTEEE","JA",JOptionPane.INFORMATION_MESSAGE);
-			vidascantidad.vidasnum=3;
 
-		}
 
 	}
 	
@@ -204,4 +257,60 @@ public class AdminitradorJuego extends JPanel implements Runnable {
 			return false;
 		}
 	}
+
+	public void ReproductorDeSonido () {
+
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(
+					AdminitradorJuego.class.getResourceAsStream("/Sonidos/musicafondo.wav"));
+
+			AudioFormat format = audioInputStream.getFormat();
+
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			clip = (Clip) AudioSystem.getLine(info);
+			clip.open(audioInputStream);
+			clip.start();
+
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void SonidoMuerte () {
+
+		try {
+			maudioInputStream = AudioSystem.getAudioInputStream(
+					AdminitradorJuego.class.getResourceAsStream("/Sonidos/bombermanmuere.wav"));
+
+			AudioFormat format = maudioInputStream.getFormat();
+
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			clipm = (Clip) AudioSystem.getLine(info);
+			clipm.open(maudioInputStream);
+			clipm.start();
+			clip.close();
+			enemigo1.evelocidad=0;
+			enemigo3.evelocidad=0;
+			enemigo2.evelocidad=0;
+			jugador.velocidad=0;
+			jugador.x=25;
+			jugador.y=25;
+			enemigo1.setEy(425);
+			enemigo1.setEx(25);
+			enemigo2.setEy(425);
+			enemigo2.setEx(925);
+			enemigo3.setEx(925);
+			enemigo3.setEy(25);
+			vidascantidad.vidasnum--;
+
+
+
+
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
 }
